7736cea7883a20ab898a2f2c5eb5a9c677e2b363
@armywalrus <armywalrus@gmail.com>
2020-08-18 11:54:37 -0400

    queries 3, 4, 6

--- homework_queries.sql
+++ homework_queries.sql
@@ -12,12 +12,36 @@ SELECT * FROM employees
 ALTER TABLE employees
 ADD PRIMARY KEY (emp_no)
 
+
 CREATE TABLE salaries (
   emp_no INT NOT NULL,
   salary INT NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 SELECT * FROM salaries
+
+
+CREATE TABLE departments (
+  dept_no VARCHAR(30) NOT NULL PRIMARY KEY,
+  dept_name  VARCHAR(30) NOT NULL
+);
+SELECT * FROM departments
+
+
+CREATE TABLE dept_managers (
+  dept_no VARCHAR(30) NOT NULL,
+  emp_no INT NOT NULL,
+  FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+SELECT * FROM dept_managers
+
+
+CREATE TABLE dept_emp (
+  emp_no INT NOT NULL,
+  dept_no VARCHAR(30) NOT NULL,
+  FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+SELECT * FROM dept_emp
 
 
 --1. List the following details of each employee: employee number, last name, first name, sex, and salary.
@@ -32,12 +56,56 @@ SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date LIKE '%1986'
 
+
+--3. List the manager of each department with the following information: department number, department name, 
+  -- the manager's employee number, last name, first name.
+CREATE VIEW manager_info AS
+SELECT d.dept_no, d.dept_name, m.emp_no
+FROM dept_managers AS m
+INNER JOIN departments AS d ON
+d.dept_no=m.dept_no;
+SELECT * FROM manager_info
+
+SELECT i.dept_no, i.dept_name, i.emp_no, e.last_name, e.first_name
+FROM employees AS e
+INNER JOIN manager_info AS i ON
+i.emp_no=e.emp_no
+
+
+--4. List the department of each employee with the following information: 
+  -- employee number, last name, first name, and department name.
+CREATE VIEW departments_info AS
+SELECT w.emp_no, w.dept_no, d.dept_name
+FROM dept_emp AS w
+INNER JOIN departments AS d ON
+w.dept_no=d.dept_no;
+SELECT * FROM departments_info
+
+SELECT e.emp_no, e.last_name, e.first_name, f.dept_name
+FROM employees AS e
+INNER JOIN departments_info AS f ON
+f.emp_no=e.emp_no
+  
 	
 --5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 SELECT first_name, last_name, sex
 FROM employees
 WHERE first_name = 'Hercules' 
 AND last_name LIKE 'B%';
+
+
+--6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, f.dept_name
+FROM employees AS e
+INNER JOIN departments_info AS f ON
+f.emp_no=e.emp_no
+WHERE dept_name = 'Sales'
+
+
+--7.List all employees in the Sales and Development departments, 
+ -- including their employee number, last name, first name, and department name.
+
+
 
 --8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 SELECT last_name, COUNT(last_name) AS "last name count"
